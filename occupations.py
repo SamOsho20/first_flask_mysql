9d16de6f8a6b8355bb556b24e88e00cfcc55954d
Sam_Osho <Sosho108@marian.edu>
2023-11-09 22:58:48 -0500

    Connecting Mysql and flask to create webpages!

--- occupations.py
+++ occupations.py
@@ -1,24 +1,27 @@
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
-class Friend:
+class occupations:
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
-        self.last_name = data['last_name']
+        self.Last_name = data['Last_name']
         self.occupation = data['occupation']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM friends;"
+        query = "SELECT * FROM occupations;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
-        results = connectToMySQL('first_flask4').query_db(query)
+        results = connectToMySQL('new_schema12').query_db(query)
         # Create an empty list to append our instances of friends
-        friends = []
+        occupations = []
         # Iterate over the db results and create instances of friends with cls.
-        for friend in results:
-            friends.append(cls(friend))
-        return friends
+        for occupation in results:
+            occupations.append(cls(occupation))
+            # watch for s in these understand difference between dictonary and list
+        return occupations
+    # Retuen a list becuase we have ran a for loop to gather 
+    # all the key value pairs in  the dictornary giving us all the values in this list 
             
--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 # import the class from friend.py
-from friend import Friend
+from occupations import occupations
 app = Flask(__name__)
 @app.route("/")
 def index():
     # call the get all classmethod to get all friends
-    friends = Friend.get_all()
-    print(friends)
-    return render_template("index.html")
+    occupationslist= occupations.get_all()
+    print(occupations)
+    return render_template("index.html", occupationslist = occupationslist)
             
 if __name__ == "__main__":
     app.run(debug=True)
